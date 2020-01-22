2ba073ec7cadcba20b1bee948b533dc073e90448
Daniel Polii <danielpolii19@gmail.com>
2020-01-22 19:48:33 +0700

    Tambah file

--- pelatihanGit.py
+++ pelatihanGit.py
@@ -1,2 +1,23 @@
-print("hellow world")
-#git . untuk menambah semua file kita \ 
+# print("hellow world")
+# #git remote add origin https://github.com/gitnyaDanil/pelatihan-git-daspro.git
+# #git . untuk menambah semua file kita harus dengan kata kerja (apa yg dilakukan)
+# #git commit -m "Initial Commit" -> misalkan Tambah nama
+# #Selanjutnya git config --global user.name "Daniel Polii"
+# mkdir namafolder
+# cd bukafolderApaa
+# git config --global user.email danielpolii19@gmail.com
+# git push origin master
+# daniels-MacBook-Air-3:clean-code danielpolii$ git config --global user.name "Daniel Polii"
+# daniels-MacBook-Air-3:clean-code danielpolii$ git config --global user.email danielpolii19@gmail.com
+# daniels-MacBook-Air-3:clean-code danielpolii$ git add
+# Nothing specified, nothing added.
+# Maybe you wanted to say 'git add .'?
+# daniels-MacBook-Air-3:clean-code danielpolii$ git push origin master
+# Username for 'https://github.com': gitnyaDanil
+# Password for 'https://gitnyaDanil@github.com':
+# git -checkout -b login -> keluar dari file dan pergi ke file login
+# git log -> untuk melihat history 
+# untuk kolakobari pergi ke setting -> collaborators (akan masuk ke email)
+# terima di email
+# git clone untuk masuk ke punya yaya
+# git push untuk menambahkan 
